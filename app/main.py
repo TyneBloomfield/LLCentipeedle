c0d19ccff09def5b2f2eae5cb1c50e01772cad30
TyneBloomfield <cat.is.fite@gmail.com>
2017-03-02 12:47:01 -0800

    skateboard snek

--- app/main.py
+++ app/main.py
@@ -1,25 +1,27 @@
 import bottle
 import os
 import random
+from pypaths import astar
 
 # data
 # example_data = {
 #     u'snakes': [
 #     {
-#         u'health_points': 100, 
-#         u'taunt': u'df0bfa20-ff6f-4054-9007-9cce441bb534 (20x20)', 
+#         u'health_points': 100,
+#         u'taunt': u'df0bfa20-ff6f-4054-9007-9cce441bb534 (20x20)',
 #         u'coords': [[1, 19], [1, 19], [1, 19]],
-#         u'name': u'battlesnake-python', 
+#         u'name': u'battlesnake-python',
 #         u'id': u'50b127ee-d802-4aad-8310-53cc3c54dbf0'
-#         }], 
-#     u'turn': 0, 
-#     u'food': [[19, 12]], 
-#     u'height': 20, 
-#     u'width': 20, 
-#     u'dead_snakes': [], 
-#     u'game_id': u'df0bfa20-ff6f-4054-9007-9cce441bb534', 
+#         }],
+#     u'turn': 0,
+#     u'food': [[19, 12]],
+#     u'height': 20,
+#     u'width': 20,
+#     u'dead_snakes': [],
+#     u'game_id': u'df0bfa20-ff6f-4054-9007-9cce441bb534',
 #     u'you': u'50b127ee-d802-4aad-8310-53cc3c54dbf0'
 # }
+
 
 def _get_me(data):
     for snake in data["snakes"]:
@@ -27,10 +29,12 @@ def _get_me(data):
             return snake
     raise Exception("I can't find myself!")
 
+
 def get_distance(point_a, point_b):
     distance_x = point_a[0] - point_b[0]
     distance_y = point_a[1] - point_b[1]
     return distance_x, distance_y
+
 
 def rate_my_food(data):
     my_snake = _get_me(data)
@@ -38,7 +42,7 @@ def rate_my_food(data):
     my_head = my_location[0]
 
     def get_food_value(food_location):
-        distance_x, distance_y =  get_distance(my_head, food_location)
+        distance_x, distance_y = get_distance(my_head, food_location)
         total = abs(distance_x) + abs(distance_y)
         return total
 
@@ -49,25 +53,78 @@ def rate_my_food(data):
     return data["food"][food_index]
 
 
-def get_next_move(data, target_location):
+def _is_occupied(data, target_location):
 
+    all_snake_points = []
+    for snake in data["snakes"]:
+        all_snake_points.append(snake["coords"])
+
+    return target_location in all_snake_points
+
+
+def get_neighbors_function(data):
+
+    def func(coord):
+        neighbor_list = [(coord[0], coord[1] + 1),
+                         (coord[0], coord[1] - 1),
+                         (coord[0] + 1, coord[1]),
+                         (coord[0] - 1, coord[1])]
+
+        height = data["height"]
+        width = data["width"]
+
+        all_snake_points = []
+        for snake in data["snakes"]:
+            for point in snake["coords"]:
+                all_snake_points.append(tuple(point))
+
+        print "Looking in %s" % all_snake_points
+
+        values = [c for c in neighbor_list
+                if c != coord
+                and c not in all_snake_points
+                and c[0] >= 0 and c[0] < width
+                and c[1] >= 0 and c[1] < height]
+
+        print "Location %s has valid locations %s" %(coord, values)
+
+        return values
+
+
+    return func
+
+
+def get_next_move(data, target_location):
     my_snake = _get_me(data)
     my_location = my_snake["coords"]
     my_head = my_location[0]
 
-    distance_x, distance_y =  get_distance(my_head, target_location)
-    
-    if abs(distance_x) > abs(distance_y):
-        if distance_x > 0:
-            return "left"
-        else:
-            return "right"
+    # neighbors = astar.grid_neighbors(data["height"], data["width"])
 
-    if distance_y > 0:
-        return "up"
-    else:
+    finder = astar.pathfinder(neighbors=get_neighbors_function(data))
+
+    # ToDo: There is a better way of doing this
+    search_start = tuple(my_head)
+    search_end = tuple(target_location)
+    total_moves, path = finder(search_start, search_end)
+
+    next_x, next_y = path[1]
+    head_x, head_y = my_head
+
+    if head_x < next_x:
+        return "right"
+
+    if head_x > next_x:
+        return "left"
+
+    # y logic is reversed, not sure why
+    if head_y < next_y:
         return "down"
 
+    if head_y > next_y:
+        return "up"
+
+    raise Exception("I don't know where to go")
 
 @bottle.route('/static/<path:path>')
 def static(path):
@@ -121,4 +178,5 @@ def move():
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 if __name__ == '__main__':
-    bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
+    bottle.run(application, host=os.getenv(
+        'IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
